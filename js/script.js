007e797434524218462f9b1d78207315f0e12557
Ibrohimjon-Kurbanov <kurbanovolddev@gmail.com>
2024-10-10 00:08:11 +0200

    4.9-dars-vazifa

---
+++ js/script.js
@@ -0,0 +1,209 @@
+// MASSIV
+
+// 1-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime"];
+
+// console.log(arr[0], arr[4]);
+
+// 2-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime"];
+// arr[4] = "salom";
+// console.log(arr);
+
+// 3-MASALA
+
+// let arr = [];
+// arr.push("Olma", "Nok", "Anor");
+// console.log(arr);
+
+// 4-MASALA
+// let arr = ["Hello", "BMW", false, 5, "Prime"].length;
+// console.log(arr);
+
+// 5-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime"];
+// arr.push("Warrior");
+// console.log(arr);
+
+// 6-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime"];
+// arr.pop();
+// console.log(arr);
+
+// 7-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime"];
+// arr.unshift("Warrior");
+// console.log(arr);
+
+// 8-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime"];
+// arr.shift();
+// console.log(arr);
+
+// 9-MASALA
+
+// let arr = [13, 24, 32, 43, 55];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// 10-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime", "Warrior"];
+
+// let str = "Warrior";
+
+// if (arr.includes(str)) {
+//   console.log("Mavjud");
+// } else {
+//   console.log("Mavjud emas");
+// }
+
+// For loop
+
+// 1-MASALA
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// 2-MASALA
+
+// let arr = ["Olma", "Nok", "Anor", "Shaftoli", "Uzum"];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// 3-MASALA
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 1; i < arr.length; i += 2) {
+//   console.log(arr[i]);
+// }
+
+// 4-MASALA
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i += 2) {
+//   console.log(arr[i]);
+// }
+
+// 5-MASALA
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -9, -12];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// 6-MASALA
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
+
+// 7-MASALA
+// let sum = 0;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum);
+
+// 8-MASALA
+// let counter = [];
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//   counter.push(arr[i] * 2);
+// }
+
+// console.log(counter);
+
+// 9-MASALA
+
+// let arr = [11, 21, 300, 42, 5, 62, 70, 81, 99, 100];
+
+// let max = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+
+// console.log(max);
+
+// 10-MASALA
+// let arr = [11, 21, 300, 42, 5, 62, 70, 81, 99, 100];
+
+// let min = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
+
+// 11-MASALA
+// let arr = [1, -2, 3, -4, 5, 6, 7, -8, 9, 10, -9, -12];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// 12-MASALA
+
+// let sum = 0;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -12, -9, -5];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     sum += arr[i];
+//   }
+// }
+
+// console.log(sum);
+
+// 13-MASALA
+
+// let arr = ["Hello", "BMW", false, 5, "Prime"];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`${i} ${arr[i]}`);
+// }
+
+// 14-MASALA
+// let arr = ["Hello", undefined, "BMW", false, 5, "Prime", true, "Warrior"];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(`${i} ${arr[i]}`);
+// }
+
+// 15-MASALA
+// let arr = ["Hello", "BMW", false, 5, "Prime", true, "Warrior"];
+
+// for (let i = 1; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
